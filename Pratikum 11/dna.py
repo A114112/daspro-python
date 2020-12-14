1cb9a5bdb9d6295aa2812fc1647c0cc24f35fd23
Hiskia Anggi <hiskianggi@gmail.com>
2020-12-14 18:14:34 +0700

    Improve dna case

--- Pratikum 11/dna.py
+++ Pratikum 11/dna.py
@@ -2,7 +2,4 @@ x = input('DNA Ayah: ')
 y = input('DNA Anak: ')
 p = sum(x[i:i+3] == y[i:i+3] for i in range(0, len(x), 3)) / (len(x) // 3) * 100
 print(p)
-if p >= 75:
-    print('dia ayahku')
-else:
-    print('dia bukan ayahku')
+print('dia ayahku') if p >= 75 else print('dia bukan ayahku')
