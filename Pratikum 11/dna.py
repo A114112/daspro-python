7a5126914a7c012d82198fd008c582506ff5de08
Hiskia Anggi <hiskianggi@gmail.com>
2020-12-14 18:13:05 +0700

    Improve dna case

--- Pratikum 11/dna.py
+++ Pratikum 11/dna.py
@@ -1,22 +1,8 @@
-def toList(val):
-    return [val[i:i+3] for i in range(0, len(val), 3)]
-
-def percentageDna(x, y):
-    x = toList(x)
-    y = toList(y)
-    l = len(x) if len(x) <= len(y) else len(y)
-    t = sum(x[i] == y[i] for i in range(l))
-    return t / l * 100
-
-def main():
-    x = input('DNA Ayah: ')
-    y = input('DNA Anak: ')
-    p = percentageDna(x, y)
-    print(p)
-    if p >= 75:
-        print('dia ayahku')
-    else:
-        print('dia bukan ayahku')
-
-if __name__ == "__main__":
-    main()
+x = input('DNA Ayah: ')
+y = input('DNA Anak: ')
+p = sum(x[i:i+3] == y[i:i+3] for i in range(0, len(x), 3)) / (len(x) // 3) * 100
+print(p)
+if p >= 75:
+    print('dia ayahku')
+else:
+    print('dia bukan ayahku')
