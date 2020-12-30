8a4f1719bed790fce4aacc9befcaebeb630b3c28
Hiskia Anggi <hiskianggi@gmail.com>
2020-12-30 19:53:13 +0700

    Improve case 2

--- Pratikum 13/case2.py
+++ Pratikum 13/case2.py
@@ -1,8 +1,2 @@
 val = input().split()
-total = 0
-for i in val:
-    total += 1
-    if i == 'E':
-        print('TIDAK LULUS')
-        exit()
-print('TIDAK LULUS') if total < 5 else print('LULUS')
+print('TIDAK LULUS') if 'E' in val or len(val) < 5 else print('LULUS')
